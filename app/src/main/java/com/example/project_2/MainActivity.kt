adf81b17468cf517f35729e53769127b5c4a5b8c
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-10-17 13:14:32 +0700

    Fungsi BasicCompose membuat layout Column yang mengisi layar penuh dan menempatkan isinya di tengah secara horizontal dan vertikal.

--- app/src/main/java/com/example/project_2/MainActivity.kt
+++ app/src/main/java/com/example/project_2/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.project_2
 
 import android.os.Bundle
+import android.text.Layout.Alignment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -28,6 +31,16 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BasicCompose(modifier: Modifier = Modifier){
+
+    Column (horizontalAlignment =  androidx.compose.ui.Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()
+    ){
+        }
 }
 
 @Composable
